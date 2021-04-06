e135f46b074ffe2b33adb5e040eab0c75fde1505
MoussaOudjama <moussa.oudjama@canal-plus.com>
2021-04-06 14:57:46 +0200

    test 2

--- EspIosConnect/HomeViewController.swift
+++ EspIosConnect/HomeViewController.swift
@@ -26,6 +26,7 @@ class HomeViewController: UIViewController, StreamDelegate {
     
     func requesterLed() {
         //test
+        //test2
         AF.request("http://10.33.1.105/led?state=\(ledState)").responseString { response in
             print(response)
         }
